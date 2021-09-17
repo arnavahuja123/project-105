c0b6574068a11b115b83e10c510dce4e75bd7ba4
arnavahuja123 <71361757+arnavahuja123@users.noreply.github.com>
2021-09-17 16:31:32 -0400

    Add files via upload

---
+++ data.py
@@ -0,0 +1,30 @@
+import math 
+import csv
+import pandas as pd
+import plotly.express as px
+with open("data.csv",newline="")as f:
+    reader=csv.reader(f)
+    file_data=list(reader)
+
+data=file_data.pop(0)
+def mean(data):
+    n=len(data)
+    total=0
+    for x in data:
+        total+=int(x)
+
+    mean=total/n
+    return mean
+
+squared_list=[]
+for number in data:
+    a=int(number)-mean(data)
+    a=a**2
+    squared_list.append(a)
+
+sum=0
+for i in squared_list:
+    sum=sum+i
+result=sum/(len(data)-1)
+std=math.sqrt(result)
+print(std)
